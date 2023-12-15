8eb9afd53959d32e67b68291f0d99cad8660bc30
Vrushank <vrushank.m@amuratech.com>
2023-12-15 22:13:02 +0530

    new data

--- project-1/src/components/Button/Button.jsx
+++ project-1/src/components/Button/Button.jsx
@@ -4,9 +4,12 @@ const Button = (props) => {
   console.log(props.isOutline);
   console.log(props.name);
   return (  
-    <button className={props.isOutline ? styles.btn_primary : styles.btn_primary_line}>
+    <button 
+    className={props.isOutline ? styles.btn_primary : styles.btn_primary_line}
+    >
     {props.icon}
-    {props.name}</button>
+    {props.name}
+    </button>
   )
 }
 
